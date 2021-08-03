726bb74525f97ca1c9fb53f0603a8d06564ceaa9
Shawn Tan <shawn@wtf.sg>
2021-08-03 15:12:07 -0400

    Preparing parser

--- src/encoder.py
+++ src/encoder.py
@@ -18,11 +18,6 @@ class Encoder(nn.Module):
         self.vocab = vocab
         self.embed = nn.Embedding(len(vocab), n_embed, vocab.pad())
         self.embed_dropout = nn.Dropout(dropout)
-        """
-        self.rnn = rnntype(
-           n_embed, n_hidden, n_layers, bidirectional=bidirectional, dropout=dropout
-        )
-        """
         self.transform = nn.Linear(n_hidden, n_layers * n_hidden)
         self.n_layers = n_layers
 
@@ -51,25 +46,3 @@ class Encoder(nn.Module):
         final_state = torch.tan(final_state).permute(1, 0, 2)
         final_state = (final_state, torch.zeros_like(final_state))
         return (flattened_internal, final_state)
-        """
-        if len(data.shape) == 3:
-            emb    = torch.matmul(data, self.embed.weight)
-            tokens = torch.argmax(data.detach(),dim=-1)
-            emb    = emb * (tokens != self.vocab.pad()).unsqueeze(2).float()
-        else:
-            # Will take this path usually.
-            emb   = self.embed(data)
-        if lens is not None:
-            padded_sequence = self.embed_dropout(emb)
-            total_length = padded_sequence.shape[0]
-            packed_sequence = nn.utils.rnn.pack_padded_sequence(padded_sequence,
-                                                                lens.cpu())
-            packed_output, hidden = self.rnn(packed_sequence)
-            output_padded,_ = nn.utils.rnn.pad_packed_sequence(packed_output,
-                                                               total_length=total_length,
-                                                               padding_value=self.vocab.pad())
-            return output_padded, hidden
-        else:
-            return self.rnn(self.embed_dropout(emb))
-
-        """
